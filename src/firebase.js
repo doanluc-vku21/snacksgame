413f66c6b21c74c2e38dd582fa6df01fddce0e3d
DoanLuc2k3 <lucdb.21it@vku.udn.vn>
2025-11-15 09:25:33 +0700

    Secure Firebase keys using .env

--- src/firebase.js
+++ src/firebase.js
@@ -23,14 +23,15 @@ import {
 } from "firebase/auth";
 
 // Cấu hình Firebase của bạn
+// Cấu hình Firebase của bạn - ĐỌC TỪ BIẾN MÔI TRƯỜNG
 const firebaseConfig = {
-  apiKey: "AIzaSyD6SiMnzg0A7l38P1ybCknweE1DFB3UnTw",
-  authDomain: "snake-game-react-297c6.firebaseapp.com",
-  projectId: "snake-game-react-297c6",
-  storageBucket: "snake-game-react-297c6.firebasestorage.app",
-  messagingSenderId: "1063536207909",
-  appId: "1:1063536207909:web:0d23180f5f56e613f737cc",
-  measurementId: "G-MV2679LE3V"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Khởi tạo Firebase
